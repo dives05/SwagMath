4c6850124121f6a9579306de71931c35ecbb9ca5
dives05 <dives@purdue.edu>
2024-07-17 23:05:12 -0400

    Search bar and scroll bar

--- script.js
+++ script.js
@@ -6,3 +6,18 @@ function goFullscreen(id) {
       element.webkitRequestFullScreen();
     }
 }
+
+function search_act() {
+  let input = document.getElementById('searchbar').value
+  input = input.toLowerCase();
+  let x = document.getElementsByClassName('actbox');
+
+  for (i = 0; i < x.length; i++) {
+    if (!x[i].innerHTML.toLowerCase().includes(input)) {
+      x[i].style.display = "none";
+    }
+    else {
+      x[i].style.display = "inline-block";
+    }
+  }
+}
